7b811c024b21d2cfe8f77bb31b4d5443c615d36c
Michelle Steigerwalt <msteigerwalt@gmail.com>
2013-08-25 16:31:41 -0700

    I really need to nest these descriptions for better test coverage.

--- spec/bsns_spec.rb
+++ spec/bsns_spec.rb
@@ -10,6 +10,9 @@ class Buzzle < BSNS::Base
 	has_many :fizzles
 	has_many :fizzles, :as => :enemies,  :with => :reason
 	has_many :fizzles, :as => :opinions, :with => :opinion
+	has_one  :cat, :embedded => true
+	has_many :fizzles, :as => :embedded_fizzles, :embedded => true
+	has_one  :wizzle, :embedded => :true
 
 end
 
@@ -18,6 +21,8 @@ class Wizzle < BSNS::Base
 	attr_accessor :name, :powers, :species, :weaknesses
 
 	acts_as_collection
+
+	has_one :fizzle
 
 end
 
@@ -63,6 +68,11 @@ describe BSNS do
 		fizz = Fizzle.load 'fizz'
 		fizz.fizzles.length.should  == 1
 		fizz.fizzles[0].name.should == "Fazzy Fazz Fazz"
+	end
+
+	it "should load embedded has_one associations" do
+		buzz = Buzzle.load 'buzz'
+		buzz.cat.thing.should == 'tiedye shirts'
 	end
 
 	it "should load self-referential associations with extra keys" do
@@ -124,6 +134,29 @@ describe BSNS do
 		fazz.cats.length.should   == 2
 		fazz.cats[0].thing.should == 'whiskers'
 		fazz.cats[1].thing.should == 'hats'
+	end
+
+	it "should load one has_many item as an array" do
+		fuzz = Fizzle.load 'fuzz'
+		fuzz.cats.length.should == 1
+		fuzz.cats[0].thing.should == 'bad breath'
+	end
+
+	it "should handle associations of embedded associations" do
+		buzz = Buzzle.load 'buzz'
+		buzz.embedded_fizzles.length.should  == 1
+		buzz.embedded_fizzles[0].name.should == "Embedded Fizzle"
+		cats = buzz.embedded_fizzles[0].cats
+		cats.length.should == 2
+		cats[0].thing.should == 'poops a lot'
+		cats[1].thing.should == 'sharp claws'
+	end
+
+	it "should handle embedded associations for has_one links" do
+		buzz = Buzzle.load 'buzz'
+		fizzle = buzz.wizzle.fizzle
+		fizzle.name.should == "Fuzzy Wuzzy"
+		fizzle.cats.length.should == 1
 	end
 
 end
